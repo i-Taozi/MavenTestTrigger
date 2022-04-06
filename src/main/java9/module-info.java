1b5c454a07353b5ba248703fe8c246e2bc171d2f
i-Taozi <879714748@qq.com>
2022-04-06 14:13:35 +0800

    bytedeco/javacpp:original code

---
+++ src/main/java9/module-info.java
@@ -0,0 +1,8 @@
+module org.bytedeco.javacpp {
+    requires jdk.unsupported;
+    exports org.bytedeco.javacpp;
+    exports org.bytedeco.javacpp.annotation;
+    exports org.bytedeco.javacpp.indexer;
+    exports org.bytedeco.javacpp.tools;
+    exports org.bytedeco.javacpp.presets;
+}
